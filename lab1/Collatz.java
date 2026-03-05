66abd84986c61168fc1e144e4abf0188418b1fec
guodegang <2710983645@qq.com>
2026-03-05 21:31:23 +0800

    22

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,7 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if(n%2==0){
             return n/2;
-        }
-        else{
-            return 3*n+1;
-        }
     }
 
     public static void main(String[] args) {
